81349cca2e8bf899e3b20efb15aa7764c94d3877
Kirill Kireev <kereeevkirill@gmail.com>
2020-11-07 01:00:08 +0300

    Begining (Add, Resize)

---
+++ Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+
+namespace DataStructures
+{
+    public class ArrayList
+    {
+        public int Length { get; private set; }
+        private int[] _array;
+
+        public ArrayList()
+        {
+            _array = new int[3];
+            Length = 0;
+        }
+        public void Add(int value)
+        {
+            if (Length > _array.Length)
+            {
+                RizeSize();
+            }
+            _array[Length] = value;
+            Length++;
+        }
+        //public override bool Equals(object obj)
+        //{
+        //    ArrayList arrayList = (ArrayList)obj;
+        //    //arrayList._array;
+        //    return true;
+        //}
+
+        private void RizeSize(int size = 1)
+        {
+            int newLength = _array.Length;
+            while (newLength < Length + size)
+            {
+                newLength = (int)(newLength * 1.33d + 1);
+            }
+
+            int[] newArray = new int[newLength];
+            Array.Copy(_array, newArray, _array.Length);
+            _array = newArray;
+        }
+    } }
